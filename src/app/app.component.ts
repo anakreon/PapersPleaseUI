da594b398b5d0375e4108f7d98ab231edd86b79a
Martin Hula <martin@anaworld.cz>
2021-03-20 11:14:12 +0100

    add PapersPlease validation library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { InputPapers, Inspector } from 'papersplease';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'PapersPleaseUI';
+    title = 'PapersPleaseUI';
+    public constructor() {
+        const bulletin = `Entrants require passport\nAllow citizens of Arstotzka, Obristan\nCitizens of Obristan require access permit`;
+
+        const inspector = new Inspector();
+        inspector.receiveBulletin(bulletin);
+        const entrant: InputPapers = {
+            passport: `ID#: asdf
+                NATION: Obristan
+                NAME: Johan Wagner
+                DOB: 1933.11.28
+                SEX: M
+                ISS: East Grestin
+                EXP: 2014.11.15`,
+        };
+        console.log('result', inspector.inspect(entrant));
+    }
 }
